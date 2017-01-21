4c5a21b2783ec00b52c0225ba031920f251af848
Kevin <mrkivinator@gmail.com>
2017-01-21 16:07:41 +0100

    Renamed BarScene to BartenderAndCustomers

--- Assets/BarManRotation.cs
+++ Assets/BarManRotation.cs
@@ -1,56 +1,67 @@
 ﻿using UnityEngine;
 
 
-public class BarManRotation : MonoBehaviour {
+public class BarManRotation : MonoBehaviour
+{
 
-	public GameObject[] rotationPoints;
-	public bool ignore;
-	public int index = 0;
-	int direction = 1;
+    public GameObject[] rotationPoints;
+    public bool ignore;
+    public int index = 0;
+    int direction = 1;
 
-	public int attentionTime = 60 * 2;
+    public int attentionTime = 60 * 2;
 
-	[Range(0f, 1.0f)]
-	public float slerpInterpolation = 0.1f;
-	public AnimationCurve slerpCurve;
+    [Range(0f, 1.0f)]
+    public float slerpInterpolation = 0.1f;
+    public AnimationCurve slerpCurve;
 
-	void Start () {
-	}
+    void Start()
+    {
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
 
-	int tick = 0 ;
+    }
 
-	public void IngnoreRotation(){
-		ignore = true;
-	}
+    int tick = 0;
 
-	public void DontIngnoreRotation(){
-		ignore = false;
-	}
+    public void IngnoreRotation()
+    {
+        ignore = true;
+    }
 
-	void LateUpdate(){
-		if (ignore){
-			return;
-		}
-		tick ++;
-		var oldRotation = transform.rotation;
-		if (tick % attentionTime == 0) {
-			if (index == rotationPoints.Length - 1){
-				direction = -1;
-			} else if (index == 0){
-				direction = 1;
-			}
-			index = index + direction; 
-		}
-		transform.LookAt(rotationPoints[index].transform);
-		transform.rotation = transform.rotation * Quaternion.Euler(0f,90f,0f);
-		var newRotation = transform.rotation;
-		float progress = ((tick % attentionTime) * 1f) / attentionTime;
-		slerpInterpolation = slerpCurve.Evaluate(progress);
-		transform.rotation = Quaternion.Slerp(oldRotation, newRotation, slerpInterpolation);
-	}
+    public void DontIngnoreRotation()
+    {
+        ignore = false;
+    }
+
+    void LateUpdate()
+    {
+        if (ignore)
+        {
+            return;
+        }
+        tick++;
+        var oldRotation = transform.rotation;
+        if (tick % attentionTime == 0)
+        {
+            if (index == rotationPoints.Length - 1)
+            {
+                direction = -1;
+            }
+            else if (index == 0)
+            {
+                direction = 1;
+            }
+            index = index + direction;
+        }
+        transform.LookAt(rotationPoints[index].transform);
+        transform.rotation = transform.rotation * Quaternion.Euler(0f, 90f, 0f);
+        var newRotation = transform.rotation;
+        float progress = ((tick % attentionTime) * 1f) / attentionTime;
+        slerpInterpolation = slerpCurve.Evaluate(progress);
+        transform.rotation = Quaternion.Slerp(oldRotation, newRotation, slerpInterpolation);
+    }
 }
