53194c8b506f422b258948ca681d145123dfbb7d
mraue <martin.raue@gmail.com>
2017-01-21 09:59:23 +0100

    Basic input loop

---
+++ Assets/Sources/Manager/GameLoopManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using GGJ2017.Shared.Logging;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+namespace GGJ2017.Game
+{
+	class GameLoopManager : MonoBehaviour
+	{
+		public Player[] players;
+
+		public bool acceptingInput;
+
+		void Update()
+		{
+			if(acceptingInput)
+			{
+				foreach (var player in players)
+				{
+					if (Input.GetKeyDown(player.assignedKey))
+					{
+						player.ShouldWave();
+					}
+				}
+			}
+		}
+	}
+}
