4af00b2f21aad77b7764f61bda35235c6c444107
Mr. Luca <luca@mrluca.com>
2017-01-21 11:21:58 +0100

    Placeholder models
    The game is done everybody pack his stuff

---
+++ Assets/BarManLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarManLogic : MonoBehaviour {
+    private Animator ani;
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Random.Range(0f, 500f) < 1f) {
+            ani.SetTrigger("Dance");
+            StartCoroutine(Idle());
+        }
+	}
+    IEnumerator Idle() {
+        yield return new WaitForSeconds(Random.Range(0.5f, 4f));
+        ani.SetTrigger("Idle");
+    }
+}
---
+++ Assets/CustomerLogic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerLogic : MonoBehaviour {
+    private Animator ani;
+    public KeyCode keypress;
+	// Use this for initialization
+	void Start () {
+        ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(keypress)) {
+            ani.SetTrigger("Wave");
+
+        }
+        if (Input.GetKeyUp(keypress))
+        {
+            ani.SetTrigger("Idle");
+
+        }
+    }
+}
