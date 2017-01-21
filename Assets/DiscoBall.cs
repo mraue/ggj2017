8b07f17f4de1c2b9a181f31a0cd8498295ff844a
Mr. Luca <luca@mrluca.com>
2017-01-21 18:18:53 +0100

    Disco Ball Stuff

---
+++ Assets/DiscoBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DiscoBall : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        transform.DORotate(new Vector3(0f, 360, 0f),speed,RotateMode.FastBeyond360).SetLoops(-1).SetEase(Ease.Linear);	
+	}
+	
+	
+    
+}
