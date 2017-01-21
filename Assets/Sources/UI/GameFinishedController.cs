c8d6b7deb2cd39cc571b553005269f7153ba9685
mraue <martin.raue@gmail.com>
2017-01-21 11:13:14 +0100

    UI: Scaffolded start and finish scene

---
+++ Assets/Sources/UI/GameFinishedController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using GGJ2017.Shared.Logging;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+namespace GGJ2017.Game
+{
+	class GameFinishedController : MonoBehaviour
+	{
+		public void Continue()
+		{
+			
+		}
+	}
+}
