133d9e2bd88cfab45e0828b083d3e7c9b7131c05
Mr. Luca <luca@mrluca.com>
2017-01-22 15:36:16 +0100

    There's a party!

---
+++ Assets/Sources/Bystander.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bystander : MonoBehaviour
+{
+
+    void Start()
+    {
+        transform.localScale = Vector3.one * Random.Range(0.9f, 1.1f);
+        StartCoroutine(StartAni());    }
+    IEnumerator StartAni()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 2f));
+        GetComponent<Animator>().SetTrigger(Random.Range(1, 7).ToString());
+    }
+}
